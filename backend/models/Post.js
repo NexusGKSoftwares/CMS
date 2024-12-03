08bb684c4be4fe20475df212f54aa2ab6ae6d5e3
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-03 14:41:57 +0300

    "Update comment for 'image' field in PostSchema"

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -4,7 +4,7 @@ const PostSchema = new mongoose.Schema({
   title: { type: String, required: true },
   slug: { type: String, required: true, unique: true },
   content: { type: String, required: true },
-  image: { type: String }, // Image path
+  image: { type: String }, // Optional image URL
   createdAt: { type: Date, default: Date.now },
 });
 
